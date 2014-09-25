5a5532bd239dbdfc7f584cfc257c48db313b4ac9
lw1582 <lw1582@nyu.edu>
2014-09-24 21:10:05 -0400

    lucy wang assignment 3

---
+++ lw1582/my_package/hello_world.py
@@ -0,0 +1,14 @@
+'''
+Created on Sep 24, 2014
+
+@author: ds-ga-1007
+'''
+def hello_world():
+    print "Hello World!"
+
+#
+# Python hello world
+#
+if __name__ == "__main__":
+    import sys
+    hello_world()
